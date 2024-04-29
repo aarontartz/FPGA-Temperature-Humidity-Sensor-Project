7adb386a6bfe919241aac6dc095f24d2b36275b4
Aaron Tartz <166546889+aarontartz@users.noreply.github.com>
2024-04-29 02:48:46 -0700

    Update top.v

--- design/top.v
+++ design/top.v
@@ -72,6 +72,54 @@ module top(
     wire nack_ack_wire;
     //=================================
     
+    wire [12:0] bram_porta_0_addr;
+    wire [31:0] bram_porta_0_din;
+    wire [31:0] bram_porta_0_dout;
+    wire bram_porta_0_en;
+    wire rst;
+    wire bram_porta_0_we;
+    
+    reg [10:0] bram_portb_0_addr = 0;
+    reg [31:0] bram_portb_0_din = 0;
+    reg bram_portb_0_we = 1'b0;
+    
+    design_2_wrapper zynq_ps_instance (
+        .BRAM_PORTA_0_addr(bram_porta_0_addr),
+        .BRAM_PORTA_0_clk(clk),
+        .BRAM_PORTA_0_din(bram_porta_0_din),
+        .BRAM_PORTA_0_dout(bram_porta_0_dout),
+        .BRAM_PORTA_0_en(bram_porta_0_en),
+        .BRAM_PORTA_0_rst(rst),
+        .BRAM_PORTA_0_we(bram_porta_0_we)
+    );
+    
+    blk_mem_gen_0 blk_mem_gen_inst (
+        .clka(clk),
+        .ena(bram_porta_0_en),
+        .wea(bram_porta_0_we),
+        .addra(bram_porta_0_addr[12:2]),
+        .dina(bram_porta_0_din),
+        .douta(bram_porta_0_dout),
+        
+        .clkb(clk),
+        .enb(1'b1),
+        .web(bram_portb_0_we),
+        .addrb(bram_portb_0_addr),
+        .dinb(bram_portb_0_din),
+        .doutb()
+    );
+    
+    always @(posedge clk) begin
+        if (bram_portb_0_addr != 11'h7ff) begin
+            bram_portb_0_addr <= bram_portb_0_addr + 1;
+            bram_portb_0_din <= meas_out_wire;          //?
+            bram_portb_0_we <= 1'b1;
+        end
+        else begin
+            bram_portb_0_we <= 1'b0;
+        end
+    end
+    
     i2c_master i2c_instance (
         .clk100MHz(clk),
         //.clk_100MHz_count(clk_count_wire),
